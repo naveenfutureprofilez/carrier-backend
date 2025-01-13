009f267a17781ca15d7690d4fe0da51a9da0f0b2
Naveen Tehrpariya <naveentehrpariya@gmail.com>
2025-01-13 22:10:40 +0530

    merge changes

--- db/config.js
+++ db/config.js
@@ -6,8 +6,8 @@ mongoose.set('strictQuery', true);
   
 
 
-mongoose.connect( `mongodb+srv://naveentehrpariya:naveentehrpariya21081998@cluster0.rnebihn.mongodb.net/carrier`, {
-// mongoose.connect(process.env.DB_URL_OFFICE, {
+mongoose.connect( `mongodb+srv://naveenfp:naveenfp@cluster0.5c8ne.mongodb.net/carrier`, {
+// // mongoose.connect(process.env.DB_URL_OFFICE, {
     useNewUrlParser: true,   
     autoIndex: false, // Don't build indexes 
     maxPoolSize: 10, // Maintain up to 10 socket connections
