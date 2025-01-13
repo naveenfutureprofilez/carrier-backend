8b37a5827c8e99ba9caeb24d59bbe264ae8f37ee
Naveen Tehrpariya <naveentehrpariya@gmail.com>
2025-01-13 22:45:19 +0530

    merge changes

--- db/config.js
+++ db/config.js
@@ -6,8 +6,8 @@ mongoose.set('strictQuery', true);
   
 
 
-mongoose.connect( `mongodb+srv://naveenfp:naveenfp@cluster0.5c8ne.mongodb.net/carrier`, {
-// // mongoose.connect(process.env.DB_URL_OFFICE, {
+// mongoose.connect( `mongodb+srv://naveenfp:naveenfp@cluster0.5c8ne.mongodb.net/carrier`, {
+mongoose.connect(process.env.DB_URL_OFFICE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     autoIndex: false, // Don't build indexes 
