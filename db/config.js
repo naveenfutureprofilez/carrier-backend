4043856f79576f540b665eace7156d89461e5eb1
Naveen Tehrpariya <naveentehrpariya@gmail.com>
2025-01-28 21:41:11 +0530

    merge changes

--- db/config.js
+++ db/config.js
@@ -11,7 +11,7 @@ mongoose.connect(process.env.DB_URL_OFFICE, {
     autoIndex: false, // Don't build indexes 
     maxPoolSize: 10, // Maintain up to 10 socket connections
     family: 4,
-    serverSelectionTimeoutMS: 15000, // Increase to 15 seconds
+    serverSelectionTimeoutMS: 25000, // Increase to 15 seconds
     socketTimeoutMS: 60000 // Increase to 15 seconds
  }).then(() => {
    console.log('MongoDB connected successfully');
